a6f8f466f10f9d298f46775a68412be3073ba2f2
nickvaningen <nickvaningen@hotmail.com>
2017-11-22 15:53:19 +0100

    scripts
    mijn scrits

---
+++ Enemy_Dead.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Enemy_Dead : MonoBehaviour
+{
+    float health;
+    float Enemyhealth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<MinionStats>().GetStat("health");
+        Enemyhealth = GameObject.FindGameObjectWithTag("Enemy").GetComponent<MinionStats>().GetStat("health");
+
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (Enemyhealth <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
---
+++ Move_Cube.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move_Cube : MonoBehaviour
+{
+    [SerializeField]
+    private float steps = 0;
+    private int chance = 0;
+
+
+    float xpos;
+    float ypos;
+
+    enum Direction {Up,Down,Left,Right, }
+
+
+
+    [SerializeField] GameObject player;
+
+    public void Down()
+    {
+        transform.position+= Vector3.down;
+        steps++;
+    }
+
+    public void Up()
+    {
+        transform.position+= Vector3.up;
+        steps++;
+    }
+    
+    public void Left()
+    {
+        transform.position+= Vector3.left;
+        steps++;
+    }
+
+    public void Right()
+    {
+        transform.position+= Vector3.right;
+        steps++;
+    }
+     void Update()
+    {
+        print(steps);
+        if (steps > 10)
+        {
+             int chance = Random.Range(0, 2);
+            if (chance == 1)
+            {
+                print("werkt");
+                SceneManager.LoadScene(1);
+            }
+            print(steps);
+            print(chance);
+            steps = 0;
+        } 
+
+    }
+
+
+
+}
+
+
